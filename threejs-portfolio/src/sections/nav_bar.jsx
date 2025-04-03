73fabdc0fdedce7963d1426740fdc3263b39dba2
SherlockPE <fabriciolopezreyes@gmail.com>
2025-04-03 12:56:01 +0200

    add new assets including images and SVGs for enhanced UI elements

--- threejs-portfolio/src/sections/nav_bar.jsx
+++ threejs-portfolio/src/sections/nav_bar.jsx
@@ -9,6 +9,11 @@ const NavBar = () => {
                     <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors" >
                         Fabricio 
                     </a>
+
+                    {/* Hamburger button */}
+                    <button>
+                        <img src={"assets/menu.svg"} alt="toogle" className="w-6 h-6"/>
+                    </button>
                 </div>
             </div>
             NavBar
